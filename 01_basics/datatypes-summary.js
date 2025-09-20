4db9a2b10d29bcc240e8322faed1081543fcc151
Pratyush12mishra <pratyushpvt17@gmail.com>
2025-09-20 01:43:15 +0000

    Added datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,23 @@
+// Primitive
+
+// 7 types: String,Number,Boolean,null,undefined,Symbol,BigInt
+
+const score=100
+const scoreless=100.3
+const isLoggedIn=false
+let userEmail;
+
+const id =Symbol('123')
+const anotherId=Symbol('123')
+console.log(id===anotherId)
+const bigNumber=3456789943244n
+
+const heros=["ironman","captain america","spiderman"]
+let myobj={
+    name:"pratty",
+    age:22,
+
+}
+// Reference(Non Primitive)
+
+// Array,Objects,Functions
