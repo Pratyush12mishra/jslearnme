a16bc28ba9b15441193eac36f1297e6fc621a584
Pratyush12mishra <pratyushpvt17@gmail.com>
2025-09-20 00:44:00 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score="33"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber)
+// "33"=> 33
+// "33abc"=>NaN
+// true=>1;false=>0
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+// console.log(Boolean(""));       // false
+// console.log(Boolean("yu"));     // true
+// console.log(Boolean(0));        // false
+// console.log(Boolean(1));        // true
+// console.log(Boolean(null));     // false
+// console.log(Boolean(undefined));// false
